07ce3ec8a61f096f50154bcddb5d860f12095568
biyash <bsstha21@gmail.com>
2024-02-29 17:22:13 +0545

    todo app

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { MdDelete } from "react-icons/md";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState(""); 
+  const [todo, setTodo] = useState("");
 
   const addTodo = () => {
     if (todo) {
@@ -17,11 +18,20 @@ export default function Todo() {
     console.log(newTodo);
   };
 
+  const handleDelete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div
-      className="flex justify-center items-center h-screen"
+      className="flex flex-col justify-center items-center h-screen"
       style={{ background: "linear-gradient(to right, #009fff, #ec2f4b)" }}
     >
+      <div className="mb-4 text-4xl text-white font-serif md:text-center mt-7 ml-3">
+        <h1>A minor todo app by Biyash Shrestha</h1>
+      </div>
       <div className="flex p-4 flex-col items-center justify-center rounded bg-[#462d38]">
         <h1 className="text-3xl text-white text-center mr-8 mb-7">
           Todo list app
@@ -42,17 +52,27 @@ export default function Todo() {
           </button>
         </div>
         <div
-          className="mt-4 flex   flex-col justify-between h-[400px]"
-          style={{ width: "300px", maxHeight: "300px", overflowY: "auto" }}
+          className="mt-4 flex flex-col justify-between h-[400px]"
+          style={{
+            width: "300px",
+            maxHeight: "300px",
+            overflowY: "auto",
+          }}
         >
-          {todos.map((item, index) => (
-            <p key={index} className=" bg-slate-500 p-2 ">
-                <ul>
-
-              {item}
-                </ul>
-            </p>
-          ))}
+          <ul>
+            {todos.map((task, index) => (
+              <li
+                key={index}
+                className="text-xl text-white mb-2 bg-slate-500 rounded-xl flex justify-between p-2"
+              >
+                <span>{task}</span>
+                <MdDelete
+                  style={{ color: "red" }}
+                  onClick={() => handleDelete(index)}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
