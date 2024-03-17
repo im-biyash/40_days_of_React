1de2371466e8735d1d8ccf8ab1bf3de69fd4f33c
biyash <bsstha21@gmail.com>
2024-03-17 20:27:05 +0545

    question data

---
+++ src/Components/Data.jsx
@@ -0,0 +1,55 @@
+const questions = [
+  {
+    question: "what is the name of capital of Australia",
+    options: ["Canberra, Sydney, Melbourne, Perth"],
+    answer: "Canberra",
+  },
+  {
+    question: "Who wrote 'To Kill a Mockingbird'?",
+    options: ["Harper Lee", "Mark Twain", "J.K. Rowling", "Ernest Hemingway"],
+    answer: "Harper Lee"
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    options: ["Mars", "Venus", "Jupiter", "Saturn"],
+    answer: "Mars"
+  },
+  {
+    question: "What is the chemical symbol for water?",
+    options: ["O2", "H2O", "CO2", "H2SO4"],
+    answer: "H2O"
+  },
+  {
+question: "Who painted the Mona Lisa?",
+    options: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Michelangelo"],
+    answer: "Leonardo da Vinci"
+  },
+  {
+    question: "Which country is home to the kangaroo?",
+    options: ["Australia", "Canada", "Brazil", "China"],
+    answer: "Australia"
+  },
+  {
+    question: "What is the largest mammal in the world?",
+    options: ["Elephant", "Blue whale", "Giraffe", "Hippopotamus"],
+    answer: "Blue whale"
+  },
+  {
+    question: "Who is credited with inventing the World Wide Web?",
+    options: ["Tim Berners-Lee", "Bill Gates", "Steve Jobs", "Mark Zuckerberg"],
+    answer: "Tim Berners-Lee"
+  },
+  {
+    question: "What is the tallest mountain in the world?",
+    options: ["Mount Kilimanjaro", "Mount Everest", "Mount Fuji", "Mount McKinley"],
+    answer: "Mount Everest"
+  },
+  {
+    question: "Which gas do plants use to carry out photosynthesis?",
+    options: ["Carbon monoxide", "Oxygen", "Nitrogen", "Carbon dioxide"],
+    answer: "Carbon dioxide"
+  }
+
+
+]
+export default questions;
